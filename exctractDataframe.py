d61a14f353f1bb8b7fa441953205ffd3d87b8af0
senbar <barsen97@gmail.com>
2018-04-06 08:27:23 -0700

    notime to explain

---
+++ exctractDataframe.py
@@ -0,0 +1,18 @@
+import csv
+
+import pandas as pd
+
+i =0
+data = []
+with open('hackathon.csv', 'rt', encoding='utf8') as csvfile:
+   spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+   for row in spamreader:
+       xx = "".join(row)
+       record = xx.split('|')
+       data.append(record)
+       print(i)
+       i+=1
+
+labels = data.pop(0)
+dataframe = pd.DataFrame.from_records(data, columns=labels)
+xx =1
